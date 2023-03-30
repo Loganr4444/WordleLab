02df2bff691240d40f2347f3ab04323cd546bbd2
Loganr4444 <55294013+Loganr4444@users.noreply.github.com>
2023-03-30 12:37:33 -0400

    Made the char variable and a for loop to iterate through the alphabet

--- Wordle.java
+++ Wordle.java
@@ -1,1 +1,11 @@
-
+public class Wordle {
+  private final String word;
+  private char[26] alphabet;
+  public Wordle(){
+    int counter;
+    for(char i = 'A'; i <= 'Z'; i++){
+      alphabet[counter] = i;
+      counter++;
+    }
+  }
+}
